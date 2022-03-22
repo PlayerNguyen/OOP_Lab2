8d22f9105d85607084dddc85ac1e1d1be47d49c4
Player Nguyên <daudaua3782@gmail.com>
2022-03-22 12:07:25 +0700

    add screenshots directory and screenshot images

--- src/question03/PersonTest.java
+++ src/question03/PersonTest.java
@@ -2,6 +2,8 @@ package question03;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class PersonTest {
 
@@ -33,6 +35,56 @@ public class PersonTest {
         for (Person person : personList) {
             System.out.println(person);
         }
+
+        // Create input interface
+        System.out.println("");
+        System.out.println("Starting insert mode...");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number of people to insert: ");
+        int numberOfPeople = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            System.out.println("Select type of person [Student/Staff]: ");
+            String mode = scanner.nextLine();
+            Person person;
+            if (mode.equalsIgnoreCase("Student")) {
+                System.out.println("What name he/her is: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Where he/her is living (address): ");
+                String address = scanner.nextLine();
+
+                System.out.println("Program studying: ");
+                String program = scanner.nextLine();
+
+                System.out.println("Year number: ");
+                int year = scanner.nextInt();
+
+                System.out.println("Fee: ");
+                double fee = scanner.nextDouble();
+                person = new Student(name, address, program, year, fee);
+            } else {
+                System.out.println("What name he/her is: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Where he/her is living (address): ");
+                String address = scanner.nextLine();
+
+                System.out.println("What school he/her is teaching: ");
+                String school = scanner.nextLine();
+
+                System.out.println("Salary pay: ");
+                double pay = scanner.nextDouble();
+
+                person = new Staff(name, address, school, pay);
+            }
+
+            System.out.println(person);
+            personList.add(person);
+        }
+
+
     }
 
 }
